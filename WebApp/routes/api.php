9a8f9935a8f0ee167f4b695cd6af51037ca2296d
Vedran Serenčeš <vedran.serences@fer.hr>
2016-12-19 18:33:14 +0100

    Added API HTTP basic auth

--- WebApp/routes/api.php
+++ WebApp/routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::get('admin', function () {
+
+    return "YEEEY";
+})->middleware('auth.basic.once');
