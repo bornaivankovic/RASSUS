8de87835c9dab751ea24b415679c4c05e07d1b50
Borna Ivanković <borna.ivankovic@fer.hr>
2017-01-07 00:53:26 +0100

    Dodan toolbar u MainActivity
    Pristup SettingsActivity-u je sada preko gumba settings u overflow meniu na toolbaru

--- MyApplication/app/src/main/java/hr/fer/tel/rassus/MainActivity.java
+++ MyApplication/app/src/main/java/hr/fer/tel/rassus/MainActivity.java
@@ -3,6 +3,9 @@ package hr.fer.tel.rassus;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +14,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(MainActivity.this,SettingsActivity.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void browse(View view){
@@ -20,11 +49,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void login(View view){
         Intent intent = new Intent(MainActivity.this,LoginActivity.class);
-        startActivity(intent);
-    }
-
-    public void settings(View view){
-        Intent intent = new Intent(MainActivity.this,SettingsActivity.class);
         startActivity(intent);
     }
 
